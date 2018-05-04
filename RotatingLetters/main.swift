6e8b29fd7d00072b3139b2ed1168539172987b9c
Zhang Matt <mzhang@lcs.on.ca>
2018-05-04 09:55:49 -0400

    thanks mr gordon

--- RotatingLetters/main.swift
+++ RotatingLetters/main.swift
@@ -13,17 +13,48 @@ var invalidCharacters = 0
 var validInput = ""
 
 //Make sure input is not nil
-while(true) {
+prompting: while true {
+    
+    // Get the raw user input
+    print("Input word is? ", terminator: "")
+    
+    // Unwrap the given input from user, make sure it is not nil
     guard let givenInput = readLine() else {
-        continue
-    }    
-    if givenInput.count < 1 || givenInput.count > 30 || givenInput == " "
-    {
-        print("input less than 1 or more than 30")
-        continue
+        
+        // Bad input, continue to next iteration of loop to prompt again
+        continue prompting
+        
     }
+    
+    // Check the input length
+    if givenInput.count < 1 || givenInput.count > 30 {
+        
+        // Bad input (too long), continue to next iteration and prompt again
+        continue prompting
+        
+    }
+    
+    // PRE-PROCESS
+    // Look at each individual character in the input
+    for individualCharacter in givenInput {
+        
+        // Check what type of character this is
+        switch individualCharacter {
+            
+        case "A", "B", "C",  "D",  "E",  "F",  "G",  "H",  "I",  "J",  "K",  "L",  "M",  "N",  "O",  "P",  "Q",  "R",  "S",  "T",  "U",  "V",  "W", "X", "Y", "Z":
+            // Acceptable character, continue checking remaining characters
+            continue
+            
+        default:
+            // Unacceptable letter, stop checking remaining characters and just prompt again
+            continue prompting
+        }
+    }
+    
+    // We got this far, so the input must be valid
     validInput = givenInput
     break
+    
 }
 
 for individualCharacter in validInput
